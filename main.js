2cbbb0127e0e5d8eb5d38c50afc652fe63803ba9
Metta Rolando <mnrolando3@gmail.com>
2022-06-06 15:44:38 -0400

    Was able to extract track name and have it display in result list. Will need to filter to only show results that match artist name.

--- main.js
+++ main.js
@@ -1,45 +1,35 @@
-const pageElement = document.querySelector('#page')
+const pageElement = document.querySelector('#page');
 
-let pageTitle = document.createElement('h1')
-pageTitle.innerText = "Music Search"
-pageElement.appendChild(pageTitle)
+let pageTitle = document.createElement('h1');
+pageTitle.innerText = "Music Search";
+pageElement.appendChild(pageTitle);
 
-let searchBox = document.createElement('input')
-searchBox.classList.add("search")
-searchBox.innerHTML = "<"
-pageElement.appendChild(searchBox)
+let searchBox = document.createElement('input');
+searchBox.classList.add("search");
+pageElement.appendChild(searchBox);
 
-let submitButton = document.createElement('button')
-submitButton.innerText = "Submit"
-
-pageElement.appendChild(submitButton)
+let submitButton = document.createElement('button');
+submitButton.innerText = "Submit";
+pageElement.appendChild(submitButton);
 //these variables are for elements on my page
 
 function buildResultList(songData) {
     console.log("Function Line 14", songData)
 
-    // songs = songData.map(function (song) {
-    //     return songData.results.trackName
-    // })
+    songData.map(function (song) {
+        console.log("Songs Line 22", song.trackName);
 
-    // console.log("Songs", songs)
-    // //this returns an array of my songs
-
-    // for (let song of songs) {
-    //     console.log("song", song)
-
-    //     let songElement = document.createElement('div')
-    //     songElement.classList.add('results')
-    //     songElement.innerText = "hello"
-
-    //     pageElement.appendChild(songElement)
-    // }
-}
+        let songElement = document.createElement('div');
+        songElement.classList.add('results');
+        songElement.innerText = `${song.trackName}, ${song.artistName}`;
+        pageElement.appendChild(songElement);
+    });
+};
 //this function builds the list of search results
 
 submitButton.addEventListener("click", function (event) {
     let userSearch = searchBox.value
-    console.log("userSearch Line 21", searchBox.value)
+    console.log("userSearch Line 21", searchBox.value);
     //this prints to the text entered by the user into the searchBox element, which occurs when the submitButton is clicked
 
     let myTunes = `https://itunes.apple.com/search?term=${userSearch}`;
@@ -48,15 +38,16 @@ submitButton.addEventListener("click", function (event) {
     fetch(myTunes, {
         method: 'GET',
         headers: { 'Content-Type': 'application/json' },
-    })
+    });
         //this gets the iTunes API data
         .then(function (response) {
-            return response.json()
-        })
+        return response.json()
+    });
         .then(function (data) {
-            console.log("Data Line 36", data)
-            buildResultList(data)
-            //this calls the buildSongList function with the promised response
-        })
+        console.log("Data Line 36", data.results)
+        //this returns an array of data results
+        buildResultList(data.results)
+        //this calls the buildSongList function with the promised response
+    });
     //fetch is nested in event listener so data is only fetched when the event occurs
-})
+});
